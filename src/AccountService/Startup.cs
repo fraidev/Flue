9355dcf62892d4c0f5e79883ec118a12cd982f27
Frai <fraifelipe@gmail.com>
2019-03-24 22:58:53 -0300

    donwgrade and dockerfile

--- src/AccountService/Startup.cs
+++ src/AccountService/Startup.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AccountService.Helpers;
@@ -5,10 +8,13 @@ using AutoMapper;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Microsoft.IdentityModel.Tokens;
 using WebApi.Services;
 
@@ -27,8 +33,7 @@ namespace AccountService
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddCors();
-            services.AddMvc()
-                .AddNewtonsoftJson();
+            services.AddMvc();
             services.AddDbContext<DataContext>(x => x.UseInMemoryDatabase("TestDb"));
             services.AddAutoMapper();
             
@@ -77,7 +82,7 @@ namespace AccountService
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             // global cors policy
             app.UseCors(x => x
@@ -97,12 +102,12 @@ namespace AccountService
 
             app.UseHttpsRedirection();
 
-            app.UseRouting(routes =>
+           /* app.UseRouter(routes =>
             {
                 routes.MapControllers();
-            });
+            });*/
 
-            app.UseAuthorization();
+            app.UseAuthentication();
 
             app.UseMvc();
         }
