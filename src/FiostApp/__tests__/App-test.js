26a25cfdf89ff05ec0b95939302cf2c36f2f5643
Frai <fraifelipe@gmail.com>
2019-04-14 19:35:43 -0300

    react works

--- src/FiostApp/__tests__/App-test.js
+++ src/FiostApp/__tests__/App-test.js
@@ -1,22 +1,14 @@
+/**
+ * @format
+ */
+
 import 'react-native';
 import React from 'react';
 import App from '../App';
+
+// Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
-import NavigationTestUtils from 'react-navigation/NavigationTestUtils';
 
-describe('App snapshot', () => {
-  jest.useFakeTimers();
-  beforeEach(() => {
-    NavigationTestUtils.resetInternalState();
-  });
-
-  it('renders the loading screen', async () => {
-    const tree = renderer.create(<App />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
-
-  it('renders the root without loading screen', async () => {
-    const tree = renderer.create(<App skipLoadingScreen />).toJSON();
-    expect(tree).toMatchSnapshot();
-  });
+it('renders correctly', () => {
+  renderer.create(<App />);
 });
