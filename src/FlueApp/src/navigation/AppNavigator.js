c1e365178d46d61817463082f5b87298a8ccdb8d
Frai <fraifelipe@gmail.com>
2019-04-15 00:07:37 -0300

    Tela de Login

--- src/FlueApp/src/navigation/AppNavigator.js
+++ src/FlueApp/src/navigation/AppNavigator.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createAppContainer, createStackNavigator, createSwitchNavigator } from 'react-navigation';
 
-import MainTabNavigator from './MainTabNavigator';
+import AuthLoadingScreen from '../screen/auth/SignInScreen';
+import AppTabNavigator from './AppTabNavigator';
+import SignInScreen from '../screen/auth/SignInScreen';
+
+const AuthStack = createStackNavigator({ SignIn: SignInScreen });
 
 export default createAppContainer(createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: MainTabNavigator,
+  AuthLoading: AuthLoadingScreen,
+  Auth: AuthStack,
+  App: AppTabNavigator,
+},
+{
+  initialRouteName: 'AuthLoading',
 }
 
 ));
