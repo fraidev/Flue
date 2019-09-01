df13b6a6124f4aff168f19f159594ead33d20827
Frai <fraifelipe@gmail.com>
2019-09-01 01:17:21 -0300

    Init

---
+++ src/Services/NotificationService/NotificationService.API/Infrastructure/AppSettings.cs
@@ -0,0 +1,8 @@
+namespace NotificationService.API.Infrastructure
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+        public string ConnectionString { get; set; }
+    }
+}
