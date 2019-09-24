06e78ab30a2534538278f604fba9aee0777738ef
Frai <fraifelipe@gmail.com>
2019-09-23 23:35:01 -0300

    Tests in UserRepository

--- src/Services/IdentityService/IdentityService.UnitTests/Domain/Repositories/UserRepositoryTests.cs
+++ src/Services/IdentityService/IdentityService.UnitTests/Domain/Repositories/UserRepositoryTests.cs
@@ -32,16 +32,63 @@ namespace IdentityService.UnitTests.Domain.Repositories
         {
             _unitOfWork.GetById<User>(_users.First().UserId).Returns(_users.First());
             var result = _sut.GetById(_users.First().UserId);
+            
             result.UserId.Should().Be(_users.First().UserId);
+            _unitOfWork.Received(1).GetById<User>(_users.First().UserId);
         }
         
         [Fact]
-        public void GetByUsernameMustReturnAUserWithThisUsername
-            ()
+        public void GetByUsernameMustReturnAUserWithThisUsername()
         {
-            _unitOfWork.GetById<User>(_users.First().UserId).Returns(_users.First());
-            var result = _sut.GetById(_users.First().UserId);
-            result.UserId.Should().Be(_users.First().UserId);
+            var result = _sut.GetByUsername(_users.First().Username);
+            
+            result.Username.Should().Be(_users.First().Username);
+            _unitOfWork.Received(1).Query<User>();
+        }
+        
+        [Fact]
+        public void GetAllMustReturnAllUsers()
+        {
+            var result = _sut.GetAll();
+            
+            result.Should().Contain(_users);
+            _unitOfWork.Received(1).Query<User>();
+        }
+        
+        [Fact]
+        public void SaveMustSaveAUsers()
+        {
+            var user = _fixture.Create<User>();
+            _sut.Save(user);
+            
+            _unitOfWork.Received(1).Save(Arg.Is<User>(x => x.UserId == user.UserId));
+        }
+        
+        [Fact]
+        public void UpdateMustUpdateAUsers()
+        {
+            var oldUserName = _users.First().Username;
+
+            var user = _users.First();
+            user.Username = _fixture.Create<string>();
+            
+            _sut.Update(user);
+            
+            _unitOfWork.Received(1).Update(Arg.Is<User>
+                (x => x.UserId == user.UserId
+                      && x.Username == user.Username
+                      && x.Username != oldUserName));
+        }
+        
+        
+        [Fact]
+        public void DeleteMustDeleteAUsers()
+        {
+            var user = _users.First();
+            _sut.Delete(user);
+            
+            _unitOfWork.Received(1).Delete(Arg.Is<User>
+                (x => x.UserId == user.UserId));
         }
     }
 }
